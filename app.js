cb7b737494d22891a52048db25accc5fd82170eb
mohamedEdhehibi <mohamed.edhehibi@tekniva.net>
2024-03-19 17:16:58 +0100

    back end

---
+++ app.js
@@ -0,0 +1,52 @@
+import express from "express";
+import mongoose from "mongoose";
+import cloudinary from "cloudinary";
+import dotenv from 'dotenv';
+import cors from "cors";
+import { errorMiddleware } from "./middlewares/error.js";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import { dbConnection } from "./database/dbConnection.js";
+import jobRouter from "./routes/jobRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import applicationRouter from "./routes/applicationRoutes.js";
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" }));
+
+// Routes
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/job", jobRouter);
+app.use("/api/v1/application", applicationRouter);
+
+// Database connection
+const mongoURI = process.env.MONGO_URI || 'mongodb+srv://ateflamloum:atefatefatef@atlascluster.gifoqjs.mongodb.net/?retryWrites=true&w=majority&appName=AtlasClu';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Error connecting to MongoDB:', err));
+dbConnection();
+
+// Cloudinary configuration
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+  api_key: process.env.CLOUDINARY_CLIENT_API,
+  api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+});
+
+// Error handling middleware
+app.use(errorMiddleware);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
+
+export default app;
